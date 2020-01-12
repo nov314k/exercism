6b5c38fdbec648dee0a5ea9f70fec961491f9088
Novak Petrovic <npetrovic@gmail.com>
2020-01-13 00:47:39 +0100

    Update solution

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,7 +1,9 @@
+DROPS = (
+   ('Pling', 3),
+   ('Plang', 5),
+   ('Plong', 7),
+)
+
 def raindrops(number):
-    factors = [(3, "Pling"), (5, "Plang"), (7, "Plong")]
-    sound = [f[1] for f in factors if number % f[0] == 0 and number != 1]
-    if sound:
-        return "".join(sound)
-    else:
-        return str(number)
+    sound = "".join(s for s, n in DROPS if not number % n)
+    return str(sound or number)
