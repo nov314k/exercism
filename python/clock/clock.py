1aad3f9ac4d1f95e18603285351af92179665940
Novak Petrovic <npetrovic@gmail.com>
2020-01-12 21:50:15 +0100

    Add solution to Clock

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,23 +1,15 @@
 class Clock:
     def __init__(self, hour, minute):
-        while hour < 0:
-            hour += 24
-        while minute < 0:
-            minute += 60
-            hour -= 1
-        self.hour = hour + minute // 60
-        self.minute = minute % 60
-        self.hour %= 24 
+        self.hour, self.min = divmod((hour*60 + minute) % (24*60), 60)
 
     def __repr__(self):
-        return f"{self.hour:02d}:{self.minute:02d}"
+        return f"{self.hour:02d}:{self.min:02d}"
 
     def __eq__(self, other):
-        pass
+        return self.hour == other.hour and self.min == other.min
 
     def __add__(self, minutes):
-        self.minute = (self.minute + minutes % 60)
-        self.hour = (self.hour + (minutes // 60)) % 24
+        return self.__class__(self.hour, self.min + minutes)
 
     def __sub__(self, minutes):
-        pass
+        return self.__class__(self.hour, self.min - minutes)
